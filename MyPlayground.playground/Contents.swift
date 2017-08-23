8448c6a2ec198ac171d775da6b9ef542122f76c5
Devin Wesley Thomas <5olidiphied@Devins-MacBook-Pro.local>
2017-08-23 09:54:22 -0400

    my first commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var myFirstInt: Int = 3
+
+for _ in 1...5 {
+    myFirstInt += 1
+    myFirstInt
+    print(myFirstInt)
+}
+for i in 1...100{
+    if i % 3 == 0 {
+        print (i)
+    }
+}
+
+var i = 1
+while i < 6 {
+    myFirstInt += 1
+    print (myFirstInt)
+    i += 1
+}
